7ccd61242e7743f0a7263ba909ed174765d2ba00
azinwiboris <azinwiboris@hotmail.com>
2023-03-20 19:49:46 -0700

    hope it works

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - this is the main
+ *
+ * Return: it is save to make it always zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
