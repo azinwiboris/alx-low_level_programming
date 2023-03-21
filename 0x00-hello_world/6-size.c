9b4e07d318df3f1c51ce15f4d00d35f79352a29a
azinwiboris <azinwiboris@hotmail.com>
2023-03-21 06:50:29 -0700

    I'll proof myself in time

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - always return void
+ *
+ * Return: return value is 0
+ */
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of a int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}
